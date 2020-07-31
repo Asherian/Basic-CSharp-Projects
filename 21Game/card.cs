5501d10cd35fd383b7fa3ce10527f4832897ef73
Asherian <Asherian@outlook.com>
2020-07-31 12:07:49 -0700

    Update
    Start of new program and example followthrough.

---
+++ 21Game/card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21Game
+{
+    public class Card
+    {
+        //creating a constructor
+        public Card()
+        {
+
+        }
+        //properties of class
+        public string Suit { get; set; }
+        public string Face { get; set; }
+
+    }
+}
---
+++ Assign161/mathing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign161
+{
+    class mathing
+    {
+        //properties of class:
+        public decimal num1 { get; set; }
+        public decimal num2 { get; set; }
+        public decimal numResult { get; set; }
+
+        //Constructor - values assigned an object upon creation.
+        public mathing()
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+    }
+}
