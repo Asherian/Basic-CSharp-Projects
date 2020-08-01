a00327eb75bee21636fb97669bc58a53ce420124
Asherian <Asherian@outlook.com>
2020-07-31 21:08:28 -0700

    Assign 165 WIP
    Functioning other than unable to accept a null value for 2nd input.

---
+++ Assign165/Options.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign165
+{
+    class Options
+    {
+        //Properties:
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int result { get; set; }
+
+        public int Mathy(int num1, int num2 = 5)
+        {
+            result = num1 + num2;
+            return result;
+        }
+
+    }
+}
---
+++ Assign165/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options opt = new Options();
+
+            Console.WriteLine("Please pick a number:");
+            opt.num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please pick a 2nd number if you'd like, else it will default to another number.");
+            int numUser = Convert.ToInt32(Console.ReadLine());
+            if (input.Length==0) 
+            {
+                opt.result = opt.Mathy(opt.num1, opt.num2 = 7);
+                Console.WriteLine("The result with the current default 7 is: " + opt.result);
+            }
+            else
+            {
+                opt.result = opt.Mathy(opt.num1, opt.num2 = numUser);
+                Console.WriteLine("The result with your number " + numUser + " is: " + opt.result);
+            }
+            Console.Read();
+        }
+    }
+}
