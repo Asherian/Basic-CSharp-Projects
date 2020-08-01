3988c5155845fedf62a62fb7058d9834fa2344bb
Asherian <Asherian@outlook.com>
2020-08-01 12:53:05 -0700

    WIP
    Not completed yet.

---
+++ Assign 169/Mathy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XmlConfiguration;
+
+namespace Assign_169
+{
+    class Mathy
+    {
+        public void div(int x)
+        {
+            int result = x / 2;
+        }
+        public static class div
+        {
+            public static string purpose = "To have a static class and fields in it.";
+            public static DateTime Today = new DateTime(2020, 08, 01);
+        }
+
+    }
+}
---
+++ Assign 169/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy m = new Mathy();
+
+            Console.WriteLine("Please choose a number to divide:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            m.div(num1);
+        }
+    }
+}
