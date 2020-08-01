6a0d6002e042bec5f8b09333f0ec44a5a543a203
Asherian <Asherian@outlook.com>
2020-08-01 12:14:51 -0700

    Assign 166
    void class exercise.

---
+++ Assign 166/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathvoid mv = new mathvoid();
+
+            int num1 = 12;
+            int num2 = 23;
+
+            mv.mathy(num1, num2);
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Assign 166/mathvoid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_166
+{
+    class mathvoid
+    {
+        public void mathy (int x, int y)
+        {
+            int result = x * x;
+            Console.WriteLine(y);
+        }
+    }
+}
