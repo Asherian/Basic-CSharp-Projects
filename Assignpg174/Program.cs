a1f3aeaf18633cb81e258b6e796948ad179bfcad
Asherian <Asherian@outlook.com>
2020-08-03 16:16:19 -0700

    Uploading Assign 174

---
+++ Assignpg174/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.FirstName = "Sample";
+            n.LastName = "Student";
+
+            n.SayName();
+
+            Console.Read();
+
+        }
+    }
+}
