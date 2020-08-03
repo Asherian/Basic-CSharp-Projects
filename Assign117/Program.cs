6147e2c5eb6f337ae89820b572956173dff7311a
Asherian <Asherian@outlook.com>
2020-08-03 16:05:16 -0700

    Assignment for 174

---
+++ Assign117/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the weight of your package?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            //string appWeight = weight >= 50 ? "The package can be shipped." : "Package too heavy to be shipped via Package Express. Have a good day.";
+            Console.WriteLine(weight);
+
+            Console.Read();
+            }
+        }
+    }
+}
