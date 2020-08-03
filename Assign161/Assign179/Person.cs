8d75424471f1f4ce223358000607e39e624fe4b5
Asherian <Asherian@outlook.com>
2020-08-03 15:58:00 -0700

    Assignment 174

---
+++ Assign161/Assign179/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign179
+{
+    public abstract class Person
+    {
+
+    }
+}
