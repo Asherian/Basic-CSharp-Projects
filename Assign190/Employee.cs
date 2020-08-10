1e012761fb2ac00af49c2f76401b0be308e11b69
Krist Asherian <asherian@outlook.com>
2020-08-10 14:20:30 -0700

    Generic created
    Still WIP - needs iteration added but generic list EmpList and EmpID have been created and don't error out.

--- Assign190/Employee.cs
+++ Assign190/Employee.cs
@@ -6,32 +6,33 @@ using System.Threading.Tasks;
 
 namespace Assign190
 {
-    class Employee : Person, IQuittable
+    class Employee<T> : Person, IQuittable
     {
+        public List<T> EmpList { get; set; }
         public int ID { get; set; }
 
         public void Quit()
         {
             Console.WriteLine("I quit, I'm done here.");
         }
-        public static bool operator ==(Employee emp1, Employee emp2)
-        {
-            if (emp1.ID == emp2.ID)
-            {
-                bool result = true;
-                return result;
-            }
-            else
-            {
-                bool result = false;
-                return result;
-            }
-        }
+//        public static bool operator ==(Employee emp1, Employee emp2)
+//        {
+//            if (emp1.ID == emp2.ID)
+//            {
+//                bool result = true;
+//                return result;
+//            }
+//            else
+//            {
+//                bool result = false;
+//                return result;
+//            }
+//        }
 
-        public static bool operator !=(Employee emp1, Employee emp2)
-        {
-            return false;
-        }
+//        public static bool operator !=(Employee emp1, Employee emp2)
+//        {
+//            return false;
+//        }
 
     }
 }
--- Assign190/Program.cs
+++ Assign190/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assign190
 {
@@ -11,43 +8,51 @@ namespace Assign190
         static void Main(string[] args)
         {
 
-            List<int> EmpList = new List<int>();
-            Employee n = new Employee();
+            List<string> EmpList = new List<string>();
+            List<int> EmpID = new List<int>();
+            Employee<string> n = new Employee<string>();
             n.FirstName = "Sample";
             n.LastName = "Student";
             n.ID = 12;
+            EmpList.Add("Sample Student");
+            EmpID.Add(12);
 
-            Employee m = new Employee();
+            Employee<string> m = new Employee<string>();
             m.FirstName = "Jesse";
             m.LastName = "James";
             m.ID = 23;
+            EmpList.Add("Jesse James");
+            EmpID.Add(23);
 
-            Employee o = new Employee();
+            Employee<string> o = new Employee<string>();
             o.FirstName = "Serious";
             o.LastName = "Business";
             o.ID = 12;
+            EmpList.Add("Serious Business");
+            EmpID.Add(12);
 
             n.SayName();
             m.SayName();
             o.SayName();
 
-            if (n.ID == m.ID)
-            {
-                Console.WriteLine(n.FirstName + " has the same Employee ID as " + m.FirstName);
-            }
-            else
-            {
-                Console.WriteLine(n.FirstName + " has a different employee ID then " + m.FirstName);
-            }
+            foreach (int i = 0; )
+            //if (n.ID == m.ID)
+            //{
+            //    Console.WriteLine(n.FirstName + " has the same Employee ID as " + m.FirstName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(n.FirstName + " has a different employee ID then " + m.FirstName);
+            //}
 
-            if (n.ID == o.ID)
-            {
-                Console.WriteLine(n.FirstName + " has the same Employee ID as " + o.FirstName);
-            }
-            else
-            {
-                Console.WriteLine(n.FirstName + " has a different employee ID then " + o.FirstName);
-            }
+            //if (n.ID == o.ID)
+            //{
+            //    Console.WriteLine(n.FirstName + " has the same Employee ID as " + o.FirstName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(n.FirstName + " has a different employee ID then " + o.FirstName);
+            //}
 
 
 
