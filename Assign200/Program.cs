a87908266d74b2a8f7d72b82def91ae33599924e
Krist Asherian <asherian@outlook.com>
2020-08-11 19:39:52 -0700

    Assign 200
    Creating a Strut and printing to screen in decimal.

---
+++ Assign200/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number ghoti = new Number();
+            ghoti.Amount = 12.42M;
+            Console.WriteLine(ghoti.Amount);
+            Console.Read();
+        }
+    }
+}
