a41a29f335ac43be552c87d3ecdf935b76270721
Asherian <Asherian@outlook.com>
2020-08-13 15:16:01 -0700

    Assign 203
    Lamda's work but the foreach can't add to a new list yet.

--- Assign203/Program.cs
+++ Assign203/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,27 +20,36 @@ namespace Assign203
                 new Employee { FirstName = "Shallan", LastName = "Davar", ID = 1 },
                 new Employee { FirstName = "Krist", LastName = "Asherian", ID = 23 },
                 new Employee { FirstName = "Adolin", LastName = "Kholin", ID = 4 },
-                new Employee { FirstName = "Robert", LastName = "Evans", ID = 121 },
-                new Employee { FirstName = "Jesse", LastName = "James", ID = 5 },
+                new Employee { FirstName = "Robert", LastName = "Evans", ID = 2 },
+                new Employee { FirstName = "Jesse", LastName = "James", ID = 6 },
                 new Employee { FirstName = "Miles", LastName = "Vorkosigan", ID = 29 },
                 new Employee { FirstName = "Cordelia", LastName = "Naismith", ID = 54 },
                 new Employee { FirstName = "Jose", LastName = "DeNada", ID = 7 },
             };
-            
+
+            Console.WriteLine("All Employees named just Joe");
+            List<Employee> empJoe = new List<Employee>();
 
             foreach (Employee person in EmpList)
             {
                 if (person.FirstName == "Joe")
                 {
                     Console.WriteLine(person.FirstName+ " "+person.LastName + " ID: "+person.ID);
+                   
                 }
             }
 
+            empJoe = EmpList.FindAll(x => x.FirstName == "Joe").ToList();
 
+            Console.WriteLine("Employees who have IDs greater than 5: ");
 
-            List<int>IDGet = EmpList.Where(Employee.ID => 5);
+            List<Employee> IDg5 = new List<Employee>();
+            IDg5 = EmpList.FindAll(x => x.ID > 5).ToList();
 
-
+            foreach (Employee person in IDg5)
+            {
+                Console.WriteLine(person.FirstName + " has an ID number of :" + person.ID);
+            }
             Console.Read();
         }
     }
