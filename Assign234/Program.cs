3cdbf0bac4425d77fc8b69592ebe948d9cb57479
Asherian <Asherian@outlook.com>
2020-08-19 21:02:59 -0700

    Assign 234
    Exception block catching.

--- Assign234/Program.cs
+++ Assign234/Program.cs
@@ -10,6 +10,28 @@ namespace Assign234
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Hello, what is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    Console.WriteLine("Please enter a valid age.");
+                   
+                }
+                else
+                {
+                    DateTime currentTime = DateTime.Now;
+
+                    Console.WriteLine("You were born in {0}", currentTime.Year - age);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter in how old you are in digits.");
+            }
+
+            Console.Read();
         }
     }
 }
